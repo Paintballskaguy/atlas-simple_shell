427b4f57bb57dd4f9781ff041fde4dd965304e6b
Paintballskaguy <Johnstournament@gmail.com>
2024-07-19 21:10:59 -0500

    updated header file shell.h

--- Test/shell.h
+++ Test/shell.h
@@ -27,6 +27,31 @@ extern pid_t pid;
 extern int numTokens;
 extern int no_reprint_prmpt;
 
+static pid_t GBSH_PID;
+static pid_t GBSH_PGID;
+static int GBSH_IS_INTERACTIVE;
+static struct termios GBSH_TMODES;
+
+static char *currentDirectory;
+extern char **environ;
+
+struct sigaction act_child;
+struct sigaction act_int;
+
+int no_reprint_prmpt;
+
+pid_t pid;
+
+/**
+ * SIGNAL HANDLERS
+ */
+/* signal handler for SIGCHLD */
+void signalHandler_child(int p);
+/* signal handler for SIGINT */
+void signalHandler_int(int p);
+
+int changeDirectory(char *args[]);
+
 /* Command struct */
 typedef struct Command {
     char *args[LIMIT];
