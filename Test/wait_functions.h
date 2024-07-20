588b1fc1bcfc034d9e0134a6ca1b16f99e0d97b2
Paintballskaguy <Johnstournament@gmail.com>
2024-07-19 21:23:35 -0500

    wait file disappeared. remade it

--- Test/wait_functions.h
+++ Test/wait_functions.h
@@ -1,9 +1,10 @@
 #ifndef WAIT_FUNCTIONS_H
 #define WAIT_FUNCTIONS_H
 
-/*#include <sys/types.h>*/
+#include <sys/types.h>
+#include <sys/wait.h>
 
-pid_t waitpid_wrapper(pid_t pid, int *status, int options);
-void wait_child_processes();
+void waitpid_wrapper(pid_t pid, int *status, int options);
+void wait_child_processes(void);
 
-#endif  /* WAIT_FUNCTIONS_H */
+#endif /* WAIT_FUNCTIONS_H */
