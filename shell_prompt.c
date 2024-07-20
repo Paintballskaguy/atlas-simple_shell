f38ca9fbe3bd1ece6522a444183b184fa842540d
Paintballskaguy <Johnstournament@gmail.com>
2024-07-20 18:19:21 -0500

    removed test folder. removed bad commands again.

--- shell_prompt.c
+++ shell_prompt.c
@@ -6,8 +6,25 @@
 
 void shell_prompt(void)
 {
-	char hostn[1204] = "";
+    char hostn[1204] = "";
+    char *logname = NULL;
+    int i;
 
-	gethostname(hostn, sizeof(hostn));
-	printf("%s@%s %s > ", getenv("LOGNAME"), hostn, getcwd(currentDirectory, 1024));
+    gethostname(hostn, sizeof(hostn));
+
+    for (i = 0; environ[i] != NULL; i++)
+    {
+        if (strncmp(environ[i], "LOGNAME=", 8) == 0)
+        {
+            logname = environ[i] + 8;
+            break;
+        }
+    }
+
+    if (logname == NULL)
+    {
+        logname = "unknown";
+    }
+
+    printf("%s@%s %s > ", logname, hostn, getcwd(currentDirectory, 1024));
 }
