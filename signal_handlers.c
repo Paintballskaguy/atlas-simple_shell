87ed7e264618905cadd58bb3051ab7191d9a5838
Paintballskaguy <Johnstournament@gmail.com>
2024-07-21 22:23:47 -0500

    updated signal to void out p int.

--- signal_handlers.c
+++ signal_handlers.c
@@ -1,18 +1,29 @@
 #include "shell.h"
 
+/**
+ * signal_handler_child - Handle SIGCHLD signal
+ * @p: Signal number
+ */
 void signal_handler_child(int p)
 {
-    wait_child_processes();
+	(void)p; /* Mark parameter as unused */
+	wait_child_processes();
 }
 
+/**
+ * signal_handler_int - Handle SIGINT signal
+ * @p: Signal number
+ */
 void signal_handler_int(int p)
 {
-    extern pid_t pid;
-    extern int no_reprint_prmpt;
+	extern pid_t pid;
+	extern int no_reprint_prmpt;
 
-    if (kill(pid, SIGTERM) == 0)
-    {
-        printf("\nProcess %d received a SIGINT signal\n", pid);
-    }
-    no_reprint_prmpt = 1;
+	(void)p; /* Mark parameter as unused */
+
+	if (kill(pid, SIGTERM) == 0)
+	{
+		printf("\nProcess %d received a SIGINT signal\n", pid);
+	}
+	no_reprint_prmpt = 1;
 }
