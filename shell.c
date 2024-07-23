98460d0bd952a74baacad76c73790d1418b91541
Paintballskaguy <Johnstournament@gmail.com>
2024-07-23 00:52:16 +0000

    stripped down the shell to bare bones. trying again

--- shell.c
+++ shell.c
@@ -1,5 +1,15 @@
 #include "shell.h"
 
+/* External declaration for environ */
+extern char **environ;
+
+/* Function prototypes */
+void init(void);
+void shell_prompt(void);
+int command_handler(char *cmd);
+char *find_command(char *command);
+void handle_signal(int sig);
+
 /**
  * init - Initialize the shell
  */
@@ -41,7 +51,12 @@ void shell_prompt(void)
 int command_handler(char *cmd)
 {
     pid_t pid;
+    char *args[2]; /* Arguments array */
     int status;
+    char *path;
+
+    args[0] = cmd;
+    args[1] = NULL;
 
     /* Fork a new process */
     pid = fork();
@@ -54,18 +69,81 @@ int command_handler(char *cmd)
     else if (pid == 0)
     {
         /* Child process */
-        char *args[] = {cmd, NULL};
-        execve(cmd, args, environ);
-        /* If execve fails */
-        perror(cmd);
+        if (strchr(cmd, '/') == NULL)
+        {
+            /* Use find_command if the command is not an absolute path */
+            path = find_command(cmd);
+            if (path == NULL)
+            {
+                fprintf(stderr, "%s: command not found\n", cmd);
+                exit(EXIT_FAILURE);
+            }
+            execve(path, args, environ);
+            free(path);
+        }
+        else
+        {
+            execve(cmd, args, environ);
+        }
+        perror("execve");
         exit(EXIT_FAILURE);
     }
     else
     {
         /* Parent process */
-        waitpid(pid, &status, 0); /* Wait for the child process to finish */
+        waitpid(pid, &status, 0);
         return 0;
     }
+}
+
+/**
+ * find_command - Finds the full path of a command
+ * @command: The command to find
+ *
+ * Return: The full path of the command, or NULL if not found
+ */
+char *find_command(char *command)
+{
+    char *path = getenv("PATH");
+    char *path_copy, *dir, *full_path;
+    int length;
+
+    if (path == NULL)
+    {
+        return NULL;
+    }
+
+    path_copy = strdup(path);
+    if (path_copy == NULL)
+    {
+        perror("strdup");
+        exit(EXIT_FAILURE);
+    }
+
+    length = strlen(path_copy) + strlen(command) + 2;
+    full_path = malloc(length);
+    if (full_path == NULL)
+    {
+        perror("malloc");
+        free(path_copy);
+        exit(EXIT_FAILURE);
+    }
+
+    dir = strtok(path_copy, ":");
+    while (dir != NULL)
+    {
+        snprintf(full_path, length, "%s/%s", dir, command);
+        if (access(full_path, X_OK) == 0)
+        {
+            free(path_copy);
+            return full_path;
+        }
+        dir = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+    free(full_path);
+    return NULL;
 }
 
 /**
--- shell.h
+++ shell.h
@@ -10,10 +10,14 @@
 #include <signal.h>
 #include <errno.h>
 
+/* External declaration for environ */
+extern char **environ;
+
 /* Function prototypes */
 void init(void);
 void shell_prompt(void);
 int command_handler(char *cmd);
+char *find_command(char *command);
 void handle_signal(int sig);
 
 #endif /* SHELL_H */
