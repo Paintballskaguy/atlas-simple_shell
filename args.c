3a50aa6f68fdeed0e147c0607ca0435f149d9e04
Paintballskaguy <Johnstournament@gmail.com>
2024-07-23 18:07:08 +0000

    trying to correct compile issues.

--- args.c
+++ args.c
@@ -8,6 +8,7 @@
 int main(int ac, char **av)
 {
     int i = 0;
+    (void)ac;  /* Cast ac to void to avoid unused parameter warning */
     while (av[i] != NULL) {
         printf("%s\n", av[i]);
         i++;
--- printenv.c
+++ printenv.c
@@ -8,6 +8,8 @@
 int main(int ac, char **av, char **env)
 {
     unsigned int i;
+    (void)ac;  /* Cast ac to void to avoid unused parameter warning */
+    (void)av;  /* Cast av to void to avoid unused parameter warning */
 
     i = 0;
     while (env[i] != NULL) {
