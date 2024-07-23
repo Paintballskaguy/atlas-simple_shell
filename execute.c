788a2e3216bd9e116dcf980e92a5782b03eec3b0
Paintballskaguy <Johnstournament@gmail.com>
2024-07-23 17:33:25 -0500

    adding a new PATH function to execute.c:

--- execute.c
+++ execute.c
@@ -6,29 +6,67 @@
  */
 void execute(char **argv)
 {
-    pid_t pid;
-    int status;
+	char *path, *cmd;
+	char *path_env = getenv("PATH");
+	char *path_dup = strdup(path_env);
+	char *dir = strtok(path_dup, ":");
+	pid_t pid;
+	int status;
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        return;
-    }
-    if (pid == 0)
-    {
-        /* Child process */
-        if (execve(argv[0], argv, NULL) == -1)
-        {
-            perror(argv[0]);
-        }
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        /* Parent process */
-        do {
-            waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+	if (access(argv[0], X_OK) == 0)
+	{
+		cmd = argv[0];
+	}
+	else
+	{
+		while (dir != NULL)
+		{
+			path = malloc(strlen(dir) + strlen(argv[0]) + 2);
+			sprintf(path, "%s/%s", dir, argv[0]);
+
+			if (access(path, X_OK) == 0)
+			{
+				cmd = path;
+				break;
+			}
+			free(path);
+			dir = strtok(NULL, ":");
+		}
+
+		if (dir == NULL)
+		{
+			fprintf(stderr, "%s: command not found\n", argv[0]);
+			free(path_dup);
+			return;
+		}
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		free(path_dup);
+		return;
+	}
+	if (pid == 0)
+	{
+		/* Child process */
+		if (execve(cmd, argv, NULL) == -1)
+		{
+			perror(argv[0]);
+		}
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		/* Parent process */
+		do
+		{
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	free(path_dup);
+	if (cmd != argv[0])
+		free(cmd);
 }
